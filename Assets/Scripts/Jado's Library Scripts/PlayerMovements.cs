501a79fedb5ee1b8f5c93756b27c0ca3770614dd
Jado <jade.bouiges@edu.esiee-it.fr>
2024-10-02 23:35:21 +0200

    feat: add movement

--- Assets/Scripts/Jado's Library Scripts/PlayerMovements.cs
+++ Assets/Scripts/Jado's Library Scripts/PlayerMovements.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovements : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   [SerializeField] private CharacterController _controller;
+   [SerializeField] private float _speed;
+
+    float turnSmoothVelocity;
+    public float turnMoveTime = 0.1f;
+    [SerializeField] private Transform _camTransform;
+
+    private void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if(direction.magnitude>= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnMoveTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDir = Quaternion.Euler(0, angle, 0) * Vector3.forward;
+            _controller.Move(direction * _speed* Time.deltaTime);
+
+        }
+
     }
 }
