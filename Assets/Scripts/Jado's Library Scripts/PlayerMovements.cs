d8fa779f304e7b1504b289c4af7c17b90c8ecc89
Jado <jade.bouiges@edu.esiee-it.fr>
2024-10-02 23:58:27 +0200

    feat: Add jump

--- Assets/Scripts/Jado's Library Scripts/PlayerMovements.cs
+++ Assets/Scripts/Jado's Library Scripts/PlayerMovements.cs
@@ -12,6 +12,9 @@ public class PlayerMovements : MonoBehaviour
     public float turnMoveTime = 0.1f;
     [SerializeField] private Transform _camTransform;
 
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField]  private bool _isOnTheFloor = true;
+
     private void Start()
     {
     }
@@ -19,6 +22,11 @@ public class PlayerMovements : MonoBehaviour
     private void Update()
     {
         MovePlayer();
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isOnTheFloor)
+        {
+            Jump();
+        }
     }
 
     private void MovePlayer()
@@ -27,16 +35,30 @@ public class PlayerMovements : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
-        if(direction.magnitude>= 0.1f)
+        //if (direction.magnitude >= 0.1f)
+        //{
+        //    float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camTransform.eulerAngles.y;
+        //    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnMoveTime);
+        //    transform.rotation = Quaternion.Euler(0, angle, 0);
+
+        //    Vector3 moveDir = Quaternion.Euler(0, angle, 0) * Vector3.forward;
+        //    _controller.Move(direction * _speed * Time.deltaTime);
+
+        //}
+
+    }
+
+    private void Jump()
+    {
+        _rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+        _isOnTheFloor = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Floor")
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camTransform.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnMoveTime);
-            transform.rotation = Quaternion.Euler(0, angle, 0);
-
-            Vector3 moveDir = Quaternion.Euler(0, angle, 0) * Vector3.forward;
-            _controller.Move(direction * _speed* Time.deltaTime);
-
+            _isOnTheFloor = true;
         }
-
     }
 }
