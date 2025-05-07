abf001c85c0a408c66ae3756a87a2c0f9ee1eb20
albanlacou <lacou.alban@gmail.com>
2025-05-07 15:20:51 +0200

    ajout des Fleurs

--- Assets/Scripts/Alban/Flowers/GrabFlower.cs
+++ Assets/Scripts/Alban/Flowers/GrabFlower.cs
@@ -8,38 +8,54 @@ public class GrabFlower : MonoBehaviour
     public Image crosshair; // Image UI servant de ligne de mire
     private Camera mainCamera;
     public float maxDistance = 100f;
-    public LayerMask targetLayer;
-
-
+    private LayerMask targetLayer = ~0;
+    private int count = 0;
+    public int maxFlowers = 2;
+    public GameObject porte;
 
     private void Start()
     {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        
     }
 
 
 
-    public int Flowers;
+    
     void Update()
     {
+        checkNbFlowers();
         CheckTargetInSight();
     }
 
+    void checkNbFlowers()
+    {
+        if(count == maxFlowers)
+        {
+            porte.SetActive(true);
+        }
+    }
+
+
     void CheckTargetInSight()
     {
-
+        
         Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, 0);
         Ray ray = mainCamera.ScreenPointToRay(screenCenter);
+       
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, maxDistance, targetLayer))
         {
+           
             if (hit.collider.gameObject.CompareTag("Flower"))
             {
+                
                 crosshair.color = Color.red;
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    Flowers++;
+                    count++;
+
                     Destroy(hit.collider.gameObject);
                     crosshair.color = Color.white;
                 }
