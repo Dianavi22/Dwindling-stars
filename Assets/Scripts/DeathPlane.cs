615a3bca0bced191150efc20d8d966ffd951a5e4
Juliette Neyrat <juliette.neyrat@edu.gobelins.fr>
2025-05-16 10:00:28 +0200

    Kill plane

---
+++ Assets/Scripts/DeathPlane.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathPlane : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Transform playerTransform;
+    private Vector3 playerPosition;
+    void Start()
+    {
+        playerPosition = playerTransform.position;
+     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerTransform.position = playerPosition;
+        }
+
+
+    }
+}
