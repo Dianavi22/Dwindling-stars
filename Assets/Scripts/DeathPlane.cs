3c6319d2b3d7fb537bb6202124ec677b2537232d
Juliette Neyrat <juliette.neyrat@edu.gobelins.fr>
2025-05-19 09:55:59 +0200

    ajustement position player

--- Assets/Scripts/DeathPlane.cs
+++ Assets/Scripts/DeathPlane.cs
@@ -8,7 +8,7 @@ public class DeathPlane : MonoBehaviour
     private Vector3 playerPosition;
     void Start()
     {
-        playerPosition =new Vector3(19, 26, 35) ;
+        playerPosition =new Vector3(27, -1, 35);
      }
 
     private void OnCollisionEnter(Collision other)
